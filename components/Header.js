7a12513cea2dc7d300f45de5cbd8c454dfc170c8
Mehrnoosh Akbarizadeh <mehrnoosh.akbarizadeh@gmail.com>
2020-04-09 15:40:37 +0430

    adding header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import {colors} from '../helpers/theme';
 
-const useStyle = makeStyle (theme => ({
+const useStyles = makeStyles (theme => ({
   root: {
-    
-  }
+    // background: ,
+    borderBottom: `1px solid ${colors.headerBorder}`,
+    height: 40,
+  },
 }));
 
 export default function Header() {
   const classes = useStyles();
-  return <Button className={classes.root}>Hook</Button>;
+  return <div className={classes.root}>Hook</div>;
 }
---
+++ helpers/theme.js
@@ -0,0 +1,4 @@
+export const colors = {
+  headerBorder: 'blue',
+  
+}
